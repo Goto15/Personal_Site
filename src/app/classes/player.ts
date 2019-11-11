61d432be4367fceee82813aaf1cea6d1ad48bae1
Ben Sullivan <ben@Bens-MacBook-Pro.local>
2019-11-11 15:23:01 -0600

    Added player/tournament classes, pages, services

---
+++ src/app/classes/player.ts
@@ -0,0 +1,5 @@
+export class Player {
+    IGN: String;
+    Elo: Number;
+    IRL_Name: String;
+}
---
+++ src/app/classes/tournament.ts
@@ -0,0 +1,5 @@
+export class Tournament {
+    Format: String;
+    Name: Text;
+    Date: Date;
+}
