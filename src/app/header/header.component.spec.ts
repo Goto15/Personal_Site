e9f4c7e0e5b1f035deb100dd9b1f20bd7282aee7
Ben Sullivan <ben@Bens-MacBook-Pro.local>
2019-11-13 16:01:51 -0600

    Added header and title unit tests

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header.component'
 
-import { HeaderComponent } from './header.component';
+//Stub Components
+@Component({ selector: 'app-title', template: ''})
+class TitleComponent {}
+
+@Component({ selector: 'app-navbar', template: ''})
+class NavbarComponent {}
+
+TestBed.configureTestingModule({
+  declarations: [
+    TitleComponent,
+    NavbarComponent
+  ]
+})
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +24,14 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      declarations: [ 
+        HeaderComponent,
+        NavbarComponent,
+        TitleComponent ]
     })
     .compileComponents();
   }));
@@ -17,9 +40,21 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('create the title', () =>{
+    const fixture = TestBed.createComponent(TitleComponent);
+    const titleComp = fixture.debugElement.componentInstance;
+    expect(titleComp).toBeTruthy();
+  })
+
+  it('create the title', () =>{
+    const fixture = TestBed.createComponent(TitleComponent);
+    const titleComp = fixture.debugElement.componentInstance;
+    expect(titleComp).toBeTruthy();
+  })
 });
--- src/app/header/title/title.component.spec.ts
+++ src/app/header/title/title.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TitleComponent } from './title.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TitleComponent', () => {
   let component: TitleComponent;
@@ -8,7 +8,8 @@ describe('TitleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TitleComponent ]
+      declarations: [ TitleComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   }));
@@ -22,4 +23,14 @@ describe('TitleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('title Benjamin', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Benjamin');
+  })
+
+  it('title Sullivan', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Sullivan');
+  })
 });
