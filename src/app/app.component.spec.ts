29a6e58b095a4adee52ffeb879d5c73ff9c03670
Ben Sullivan <ben@Bens-MacBook-Pro.local>
2019-11-13 10:33:48 -0600

    Added app.component unit tests. Cleaned up app.html along the way

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,32 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import { Component } from '@angular/core';
+
+
+
+//Stubbing Components for unit tests
+@Component({ selector: 'app-header', template: ''})
+class HeaderComponent {}
+
+TestBed.configureTestingModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ]
+})
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
       ],
     }).compileComponents();
   }));
@@ -20,16 +37,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'psite'`, () => {
+  it(`should have as title 'Benjamin Sullivan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('psite');
+    expect(app.title).toEqual('Benjamin Sullivan');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to psite!');
-  });
 });
